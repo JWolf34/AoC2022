77f24cc7e984ecc592fcde99e35bbc672d922a8a
John Wolf <johnmwolf34@gmail.com>
2022-11-29 15:54:02 -0600

    initial commit
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   fetchInput.py

---
+++ fetchInput.py
@@ -0,0 +1,30 @@
+import datetime
+import requests
+import json
+
+def fetchInput():
+    date = str(datetime.date.today())
+    date = date.split('-')
+    YEAR = date [0]
+    MONTH = date [1]
+    DAY = date[2]
+
+    print("Fetching input for Day {day} of year {year}...".format(day = DAY, year = YEAR))
+    session_is = getSession()
+    cookies_are = dict(session=session_is)
+    r = requests.get("https://adventofcode.com/{year}/day/{day}/input".format(year = YEAR, day = DAY), cookies=cookies_are)
+    input = open("Day{day}\input.txt".format(day = DAY), 'a')
+    input.write(r.text)
+    input.close()
+
+    print("Fetched! Happy coding :)")
+
+def getSession():
+    with open("secrets.json") as secrets:
+        data = json.load(secrets)
+    return data['session']
+
+if __name__ == '__main__':
+    fetchInput()
+
+    
