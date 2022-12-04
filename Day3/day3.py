cd429bfad644226131821f91738f0cfce7fdb38e
BuildTools <unconfigured@null.spigotmc.org>
2022-12-04 12:06:52 -0600

    day 3 input

---
+++ Day3/day3.py
@@ -0,0 +1,15 @@
+import os
+
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__),"input.txt"))) as f:
+    input = f.read().splitlines()
+
+def part1():
+    for rucksack in input:
+        comp_1 = rucksack[]
+
+def part2():
+    pass
+
+if __name__ == '__main__':
+    part1()
+    part2()
